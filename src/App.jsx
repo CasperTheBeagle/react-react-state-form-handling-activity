ac5cedfc434a4786e68ea2822fcadc737829973b
nattawara <nattawara.jtk@gmail.com>
2025-08-06 15:52:38 +0700

    add init code

--- src/App.jsx
+++ src/App.jsx
@@ -1,10 +1,18 @@
+import { useState } from "react";
 import "./App.css";
 
 function App() {
   return (
-    <>
+    <div className="App">
       <h1>Email Validation App</h1>
-    </>
+      <form className="email-container">
+        <h2 style={{ color: "red" }}>ผลลัพธ์จะมาแทนที่ตรงนี้</h2>
+        <input type="text" placeholder="Enter your email" />
+        <div className="buttons">
+          <button type="submit">Validate</button>
+        </div>
+      </form>
+    </div>
   );
 }
 
