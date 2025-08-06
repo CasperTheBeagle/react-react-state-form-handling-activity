0ce2ada53c800a3303a51d6bd117375ad4da4ddd
nattawara <nattawara.jtk@gmail.com>
2025-08-06 15:50:54 +0700

    add init code

--- src/App.jsx
+++ src/App.jsx
@@ -1,10 +1,18 @@
+import { useState } from "react";
 import "./App.css";
 
 function App() {
   return (
-    <>
-      <h1>Simple Greeting App</h1>
-    </>
+    <div className="App">
+      <h1>Simple Greeting Message App</h1>
+      <div className="greeting-container">
+        <h2>ผลลัพธ์จะมาแทนที่ข้อความนี้</h2>
+        <div className="buttons">
+          <button>Hi</button>
+          <button>สวัสดี</button>
+        </div>
+      </div>
+    </div>
   );
 }
 
