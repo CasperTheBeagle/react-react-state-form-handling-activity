97dc337db8788e0d8989219e5fcc2c48ee01897b
CasperTheBeagle <146039265+CasperTheBeagle@users.noreply.github.com>
2026-01-08 10:09:22 +0700

    update: greeting function

--- src/App.jsx
+++ src/App.jsx
@@ -2,14 +2,24 @@ import { useState } from "react";
 import "./App.css";
 
 function App() {
+  const [greeting, setGreeting] = useState("ผลลัพธ์จะมาแทนที่ข้อความนี้");
+
+  const handleHi = () => {
+    setGreeting("Hi");
+  };
+
+  const handleSawasdee = () => {
+    setGreeting("สวัสดี");
+  };
+
   return (
     <div className="App">
       <h1>Simple Greeting Message App</h1>
       <div className="greeting-container">
-        <h2>ผลลัพธ์จะมาแทนที่ข้อความนี้</h2>
+        <h2>{greeting}</h2>
         <div className="buttons">
-          <button>Hi</button>
-          <button>สวัสดี</button>
+          <button onClick={handleHi}>Hi</button>
+          <button onClick={handleSawasdee}>สวัสดี</button>
         </div>
       </div>
     </div>
