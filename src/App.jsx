ef0f95a78681aff2717adc590bcaee96189d484a
CasperTheBeagle <146039265+CasperTheBeagle@users.noreply.github.com>
2026-01-08 10:46:22 +0700

    feat: e-mail validation

--- src/App.jsx
+++ src/App.jsx
@@ -2,12 +2,32 @@ import { useState } from "react";
 import "./App.css";
 
 function App() {
+  const [email, setEmail] = useState("");
+  const [message, setMessage] = useState("");
+
+  const validateEmail = (e) => {
+    e.preventDefault();
+    const emailRegex = /^[^\s@]+@[^\s@]+\.[^\s@]+$/;
+    if (emailRegex.test(email)) {
+      setMessage("Email is valid");
+    } else {
+      setMessage("Email is not valid");
+    }
+  };
+
   return (
     <div className="App">
       <h1>Email Validation App</h1>
-      <form className="email-container">
-        <h2 style={{ color: "red" }}>ผลลัพธ์จะมาแทนที่ตรงนี้</h2>
-        <input type="text" placeholder="Enter your email" />
+      <form className="email-container" onSubmit={validateEmail}>
+        <h2 style={{ color: message === "Email is valid" ? "green" : "red" }}>
+          {message}
+        </h2>
+        <input
+          type="text"
+          placeholder="Enter your email"
+          value={email}
+          onChange={(e) => setEmail(e.target.value)}
+        />
         <div className="buttons">
           <button type="submit">Validate</button>
         </div>
